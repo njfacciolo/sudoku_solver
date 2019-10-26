ece3f60af097db5725dc6afa4193284337751c5d
FaccioloN <nick.facciolo@formulatrix.com>
2019-10-25 20:41:41 -0400

    extracts the sudoku board from my single image

--- puzzle_image_extractor.py
+++ puzzle_image_extractor.py
@@ -96,11 +96,13 @@ def _apply_morphology(img):
 
     return closed
 
+
 def _get_contours(img):
     contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     to_display = cv2.drawContours(_safe_convert_to_color(img), contours, -1, (0, 255, 0), 2)
     _show_image(to_display)
     return contours
+
 
 def _filter_contours(img, contours):
     rows,cols = img.shape[:2]
@@ -116,22 +118,103 @@ def _filter_contours(img, contours):
 
     epsilon = rows/50 if rows < cols else cols/50
 
-
     aproximated = cv2.approxPolyDP(largest_contour, epsilon, closed=True)
     to_display = cv2.drawContours(_safe_convert_to_color(img), largest_contour, -1, (0, 255, 0), 3)
 
-    # aproximated[:,0,:] returns an n row by 2 col array of points
-    # first index changes the row (set of x, y pair)
-    # last index selects column, either x or y
-
     aprox_len = len(aproximated[:])
+    ret = []
     for i in range(aprox_len):
         pt_row, pt_col = aproximated[i][0][0], aproximated[i][0][1] # What is this crap...
+        ret.append((pt_row, pt_col))
         cv2.circle(to_display, (pt_row, pt_col), 3, (0,0,255), -1)
 
     _show_image(to_display)
 
-    return largest_contour
+    return ret
+
+
+def _get_singles(img_gray, corners):
+    corners = sorted(corners)
+
+    top_left = corners[0]
+    top_right = corners[1]
+    if top_left[1] > top_right[1]:
+        top_left = corners[1]
+        top_right = corners[0]
+
+    bottom_left = corners[2]
+    bottom_right = corners[3]
+    if bottom_left[1] > bottom_right[1]:
+        bottom_left = corners[1]
+        bottom_right = corners[0]
+
+    # print(corner for corner in corners)
+
+    return 0
+
+
+def _order_points(pts):
+    # Originally from: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+    # Light cleanup
+    ret = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    ret[0] = pts[np.argmin(s)]
+    ret[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    ret[1] = pts[np.argmin(diff)]
+    ret[3] = pts[np.argmax(diff)]
+
+    return list(ret)
+
+def _four_point_transform(image, pts):
+    # Originally from: https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/
+
+    pts = np.asarray([np.array(pt) for pt in pts], dtype = np.float32)
+
+    new_width = min(Image_Displayer.DISPLAY_SIZE)
+    new_height = new_width
+
+    dst = np.array([
+        [0, 0],
+        [0, new_width - 1],
+        [new_width - 1, new_height - 1],
+        [new_height - 1, 0]], dtype=np.float32)
+
+    # print(dst)
+    # print(pts)
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(pts, dst)
+    warped = cv2.warpPerspective(image, M, (new_width, new_height))
+
+    # return the warped image
+    _show_image(warped)
+    return warped
+
+def _get_values(img):
+    # Img should be de-warped and square already. Goal is to now divide it into
+    # even blocks and perform number extraction
+
+    rows,cols = img.shape[:2]
+    ret = []
+    if rows != cols:
+        return ret
+
+    step_size = rows/9
+
+    colored = _safe_convert_to_color(img)
+
+    for row in range(1, 9):
+        cv2.line(colored, (int(row*step_size), 0), (int(row*step_size), cols-1), (0,255,0), 2)
+    for col in range(1, 9):
+        cv2.line(colored, (0, int(col*step_size)), (rows-1, int(col*step_size)), (0,255,0), 2)
+    # for row in range(9):
+        # for col in range(9):
+
+    _show_image(colored)
+
 
 
 
@@ -164,12 +247,21 @@ def extract_puzzle(img, display_step_time=200):
 
     puzzle_region = _filter_contours(morphology, contours)
 
+    if puzzle_region is None or len(puzzle_region) != 4:
+        print('Failed to find any suitable contours representative of a sudoku board.')
+        return ret  # Empty board
+
+    # Exterior has been found
+    # Find the interior regions? -> This would require unbroken perimeters. Unlikely
+
+    transformed = _four_point_transform(gray, puzzle_region)
+
+    values = _get_values(transformed)
 
 
 
-    # Contour Filtering  - find 4 sided figures that take up a majority of the frame?
 
-    # cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
 
 
 # Test
